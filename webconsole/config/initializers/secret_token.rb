45d088fcdf74ec38555c39203cafcc9c041d291f
Koichi Fujikawa <fujikawa@cirius.co.jp>
2010-05-15 22:28:11 +0900

    rails 3 frontend webapp added.

---
+++ webconsole/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '56a819424c53a7317be7f8f38871173f42ed4b069e1c1f001cb2a280e13c5783022c9de5b3df86ed63c91065a5aae2d543aa323e5af3649d93409a67a4995fe8'
